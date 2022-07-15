de62e40f60bf74d020170e162f33402987780197
RostyslavWeb <rossmysiuga@gmail.com>
2022-07-15 13:30:31 -0700

    submenu and final navigation

--- src/js/main.js
+++ src/js/main.js
@@ -30,3 +30,19 @@ function burgerMenu() {
     })
   }
   burgerMenu()
+
+// Submenu
+
+function submenu(){
+  const submenu = document.querySelector('.submenu');
+  const trigger = document.querySelector('.with__submenu');
+  const triggerLink = document.querySelector('.trigger__link');
+  const arrow = document.querySelector('.submenu__arrow');
+
+  trigger.addEventListener('click', () =>{
+    submenu.classList.toggle('active');
+    triggerLink.classList.toggle('active');
+    arrow.classList.toggle('active');
+  });
+}
+submenu();
